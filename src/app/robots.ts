510645797c2801ff64976cf6e5fb6b6bde0d3ad3
shankarportfolio <shankar.g061195@gmail.com>
2026-03-06 19:56:06 +0530

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots{
             userAgent: "*",
             allow: "/",
         },
-        sitemap: "https://navviewapp.vercel.app/sitemap.xml"
+        sitemap: "https://navview.vercel.app/sitemap.xml"
     };
 }
