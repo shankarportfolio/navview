056bfd7500328786843e5ec2bdd6e480da9ac583
shankarportfolio <shankar.g061195@gmail.com>
2026-03-07 10:22:00 +0530

    Update PageSection.tsx

--- src/app/ui/components/PageSection.tsx
+++ src/app/ui/components/PageSection.tsx
@@ -7,7 +7,6 @@ export const PageSection = ({children, CustomClasses} : PageSectionType) =>{
     return(
         <section className={`w-full h-[calc(100vh-221px)] mt-17.25 py-8.75 px-5 overflow-y-auto ${CustomClasses ? CustomClasses : ''}`} >
             <div className="w-full lg:max-w-6xl m-auto">
-                <div className="bg-red-500 text-white">TEST DEPLOY</div>
                 {children}
             </div>
         </section>
