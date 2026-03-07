77822b2a787b04cf1b3521ecee3222bb5566ff6a
shankarportfolio <shankar.g061195@gmail.com>
2026-03-07 09:50:15 +0530

    Update PageSection.tsx

--- src/app/ui/components/PageSection.tsx
+++ src/app/ui/components/PageSection.tsx
@@ -5,7 +5,7 @@ type PageSectionType = {
 
 export const PageSection = ({children, CustomClasses} : PageSectionType) =>{
     return(
-        <section className={`w-full h-[calc(100vh-171px)] mt-17.25 py-8.75 px-5 overflow-y-auto ${CustomClasses ? CustomClasses : ''}`} >
+        <section className={`w-full h-[calc(100vh-251px)] mt-17.25 py-8.75 px-5 overflow-y-auto ${CustomClasses ? CustomClasses : ''}`} >
             <div className="w-full lg:max-w-6xl m-auto">
                 {children}
             </div>
